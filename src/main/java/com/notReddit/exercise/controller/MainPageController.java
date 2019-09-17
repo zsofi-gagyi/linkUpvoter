93aa94348dee9c9ebe431c58cbb1f007dc783038
Zsófi Gagyi <gagyizsofi@gmail.com>
2019-09-17 12:49:16 +0200

    refactor(getPage): pass possible page numbers as list to thymeleaf

--- src/main/java/com/notReddit/exercise/controller/MainPageController.java
+++ src/main/java/com/notReddit/exercise/controller/MainPageController.java
@@ -36,6 +36,7 @@ public class MainPageController {
     model.addAttribute("userId", userId);
     model.addAttribute("posts", postsToBeDisplayed);
     model.addAttribute("postsPerPage", postsPerPage);
+    model.addAttribute("possiblePostsPerPage", new int[] {2, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27});
     model.addAttribute("pageLinks",  pageView.pageNumbers);
 
     return "main";
